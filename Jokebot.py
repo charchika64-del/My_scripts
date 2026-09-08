d95bdc98e862fd482eef5654919388191ecccff9
Charchika <charchika64@gmail.com>
2026-09-08 16:50:10 +0530

    Create Jokebot.py

---
+++ Jokebot.py
@@ -0,0 +1,14 @@
+import requests 
+url="https://official-joke-api.appspot.com/random_joke"
+seen_jokes=[]
+#it has keys type,setup,punchline,id
+permission=input("Wanna laugh(y/n)? ")
+while permission=="y":
+    joke=requests.get(url)
+    joke=joke.json()
+    print(f"Computer: {joke["setup"]}")
+    reply=input("You: ")
+    print(f"Computer: {joke["punchline"]}")
+    permission=input("Wanna laugh(y/n)? ")
+else:
+    print("Computer: Who will listen my jokes? Okay bye.")
